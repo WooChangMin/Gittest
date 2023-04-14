fb2848be66449da919b3ce3abfc39a1d044483b8
woochangmin <rebuilding33@gmail.com>
2023-04-14 17:38:14 +0900

    잘못만들었따

--- Gittest/Program.cs
+++ Gittest/Program.cs
@@ -1,12 +1,13 @@
-﻿namespace Gittest
+﻿using System.Collections.Concurrent;
+
+namespace Gittest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("안녕");
-
-
+            Console.WriteLine("굉장한 기능 개발1");
+            Console.WriteLine("앗 잘못만들었따");
         }
     }
 }
